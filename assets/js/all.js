bc476b0a66cad1f11872082a00bc586b44d5f367
Lina-SHU <sakura0477@gmail.com>
2022-09-04 13:00:07 +0800

    Update 2022-09-04T04:59:57.122Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,10 @@
-$(document).ready(() => {
+"use strict";
+
+$(document).ready(function () {
   if ($('.grid')) {
     $('.grid').imagesLoaded().progress(function () {
       $('.grid').masonry(); // 渲染整體畫面
     });
   }
 });
+//# sourceMappingURL=all.js.map
