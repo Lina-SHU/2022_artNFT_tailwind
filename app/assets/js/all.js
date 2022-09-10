4df0f9bbac0f117e29d7f45069466378e3ed8604
Lina-SHU <sakura0477@gmail.com>
2022-09-10 17:58:57 +0800

    add search page

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -78,4 +78,12 @@ $(document).ready(() => {
     $('.historyBtn .fa-angle-down').toggleClass('hidden');
   })
   /* 藝術品介紹 end */
+
+  /* 藝術品系列-篩選 */
+  $('.searchArtistBtn').click(function(e) {
+    e.preventDefault();
+    $('.searchArtist').slideToggle();
+    $('.mainContent').toggleClass('hidden');
+  })
+  /* 藝術品系列-篩選 end */
 });
